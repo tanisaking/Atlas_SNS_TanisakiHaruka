9f4bf4c2575ed9ecfd5472d285b2170109d4beac
KB00007 <>
2022-10-08 14:00:36 +0900

    投稿ホーム設置 投稿機能の実装

--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;//13行目の$posts=Post::get();のPostを定義
+use App\User;
+use Auth;
+use Validator;
 
 class PostsController extends Controller
 {
     //
-    public function index(){
-        return view('posts.index');
+    public function index()
+    {
+        //全ての投稿を取得
+        $posts = Post::get();//Postテーブルが情報をgetした時値を$postとして入手する
+
+        return view('posts.index',[
+            'posts' => $posts
+        ]);
+    }
+    
+    public function store(Request $request){
+
+        //バリデーション
+        //$varidator = Varidatator::make($request->all(),[
+            //'post_title' => 'request|max:255'
+            //'post_content => 'required|max:255'
+        //]);
+
+        //バリデーションエラー
+        //if ($varidator->fails()){
+            //return redirect('/') 
+            //       ->withInput
+            //       ->withErrors($varidator);
+        //}
+
+        //以下に登録処理を記述　(Eloquentモデル)
+            $posts = new Post;//新しい投稿を$postとし
+            $posts->post = $request->post;//投稿をpostテーブルに
+            $posts->user_id = Auth::id();//ここでログインしているユーザーidを登録
+            $posts->save();
+
+            return redirect('/top');
+
     }
 }
